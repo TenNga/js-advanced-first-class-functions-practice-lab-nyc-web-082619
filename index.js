dd307951c033fa5e1b95bb57ebad1acd83aabbbc
Kevin Karma <kevinkarma55@gmail.com>
2019-10-07 20:53:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function logDriversByHometown(drivers,location){
 }
 
 function driversByRevenue(drivers) {
-  return drivers.sort((a,b)=>{return a.revenue-b.revenue})
+  return drivers.slice().sort((a,b)=>{return a.revenue-b.revenue})
 }
